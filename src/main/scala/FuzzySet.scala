f2a1752c2a05e9a68cd4d5f69419ce9091c1d6fa
andre <andreq11293@gmail.com>
2026-01-06 16:09:02 +0300

    added iterator for FuzzyCollection

--- src/main/scala/FuzzySet.scala
+++ src/main/scala/FuzzySet.scala
@@ -1,23 +1,59 @@
 package hidewise.fuzzy_logic
 
-//trait FuzzySet[-A] extends (A => FuzzyValue)
-sealed trait FuzzyCollection[A]
+sealed trait FuzzyCollection[+A]
 
 case object FEmpty extends FuzzyCollection[Nothing]
 
-case class FCons[A](
-  head: FuzzyElement[A],
-  tail: FuzzyCollection[A]
-) extends FuzzyCollection[A]
+class FCons[A](
+  private val _head: FuzzyElement[A],
+  private val _tail: FuzzyCollection[A]
+) extends FuzzyCollection[A] with Seq[FuzzyElement[A]] {
+
+  private val _iterator: Iterator[FuzzyElement[A]] = new Iterator[FuzzyElement[A]] {
+    private var current: FuzzyCollection[A] = FCons.this
+
+      override def hasNext: Boolean = _tail match {
+        case FEmpty => false
+        case cons: FCons[_] => true
+      }
+
+      override def next(): FuzzyElement[A] = _tail match {
+        case FEmpty => throw new NoSuchElementException("next on empty iterator")
+        case cons: FCons[A] =>
+          current = cons._tail
+          cons._head
+      }
+    }
+
+  override def length: Int = _tail match {
+    case FEmpty => 1
+    case cons: FCons[A] => 1 + cons.length
+  }
+
+  override def iterator: Iterator[FuzzyElement[A]] = _iterator
+
+  override def apply(i: Int): FuzzyElement[A] =
+    if (i < 0) throw new IndexOutOfBoundsException(i)
+    else if (i == 0) _head
+    else _tail match {
+      case FEmpty => throw new IndexOutOfBoundsException(i)
+      case cons: FCons[A] => cons(i - 1)
+    }
+}
 
 
 object FuzzyCollection{
 
-  extension[A](seq: Seq[Double])
-    def toFuzzy(mFunction: MembershipFunction) = seq.map(mFunction)
+  extension(seq: Seq[Double])
+    def toFuzzy(mFunction: MembershipFunction) = seq.foldLeft[FuzzyCollection[Double]](FEmpty){ case (acc, value) =>
+      FCons(value -> mFunction(value), acc)
+    }
 
   extension[A](seq: Seq[A])
-    def toFuzzy(f: A => Double)(mFunction: MembershipFunction): Seq[FuzzyElement[A]] = seq.map( v => v -> mFunction(f(v)))
+    def toFuzzy(f: A => Double)(mFunction: MembershipFunction): FuzzyCollection[A] =
+      seq.foldLeft[FuzzyCollection[A]](FEmpty){ case (acc, value) =>
+        FCons(value -> mFunction(f(value)), acc)
+      }
 
   def fuzzify[A](value: Double, mFunction: MembershipFunction): FuzzyValue = mFunction(value)
 }
